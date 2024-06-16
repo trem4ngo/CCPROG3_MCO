8492e8b2eda5a2292815743879656c411dcd66f6
DouiJ <141159120+DouiJ@users.noreply.github.com>
2024-06-17 02:25:08 +0800

    Update Room.java

--- Room.java
+++ Room.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 
 class Room {
 
+    private ArrayList<Reservation> reservationList; // Stores the reservation list -- static is used to be used in other classes
     private final String roomName;
     private double basePrice;
     private boolean isReserved;                     // Just signifies if just one day is reserved for ease
-    private static ArrayList<Reservation> reservationList; // Stores the reservation list -- static is used to be used in other classes
     private int[] daysReserved;                     // 1 isReserved, 2 isCheckInDate, 3 isCheckOutDate, 4 isOverlap, 0 isNotReserved
 
     public Room(String roomName) {
@@ -16,31 +16,35 @@ class Room {
         this.daysReserved = new int[31];
     }
 
+    public ArrayList<Reservation> getReservations() {
+        return reservationList;
+    }
+
     public String getRoomName() {
         return this.roomName;
     }
 
-    public void setReservationList(int rTag, int checkInDate,int checkOutDate) {  // Sets all to 0 not reserved
+    public void setReservationList(int rTag, int checkInDate, int checkOutDate) {  // Sets all to 0 not reserved
         int i;
 
-        if(rTag !=0){
-            for (i = checkInDate-1; i < checkOutDate; i++){
-                if(i == checkInDate-1)
+        if (rTag != 0) {
+            for (i = checkInDate - 1; i < checkOutDate; i++) {
+                if (i == checkInDate - 1)
                     this.daysReserved[i] = 2;
-                else if(i == checkOutDate-1)
+                else if (i == checkOutDate - 1)
                     this.daysReserved[i] = 3;
-                else if(this.daysReserved[i] == 3 || this.daysReserved[i] == 2)
+                else if (this.daysReserved[i] == 3 || this.daysReserved[i] == 2)
                     this.daysReserved[i] = 4;
                 else
                     this.daysReserved[i] = 1;
-        }else{
-            for (i = checkInDate-1; i < checkOutDate; i++)
+            }
+        }
+
+        if (rTag == 0)
+        {
+            for (i = checkInDate - 1; i < checkOutDate; i++)
                 this.daysReserved[i] = 0;
         }
-    }
-
-    public static ArrayList<Reservation> getReservations() {
-        return reservationList;
     }
 
     public int[] getDaysReserved() {
@@ -69,6 +73,8 @@ class Room {
             count += reservation.getCheckOutDate() - reservation.getCheckInDate() + 1;
         }
         return count;
-    } 
+    }
+
+    // Create a method to check days reserved and if they are all 0, set reserved to false again
 
 }
