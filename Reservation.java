b1e08f465f7506ef864dcac0b4911691a8efea84
trem4ngo <tremaynedelacruz14@gmail.com>
2024-06-16 22:06:16 +0800

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -53,34 +53,6 @@ class Reservation {
         double basePrice = this.room.getBasePrice();
         return "Total Price Breakdown: " + "You reserved for a total of " + numOfDays + " days. Days * $" + basePrice + " per night = $" + this.totalPrice;
     }
-    
-    // Reserves the days that customers reserved. 1 isReserved, 2 isCheckInDate, 3 isCheckOutDate, 4 isOverlap
-    public void addReservation() {
-        int i;
-        int cID = reservation.getCheckInDate()-1;
-        int cOD = reservation.getCheckOutDate()-1;
-
-        for (i = cID; i < cOD; i++){
-            if(i == cID)
-                daysReserved[i] = 2;
-            else if(i == cOD)
-                daysReserved[i] = 3;
-            else if(daysReserved[i] == 3 || daysReserved[i] == 2)
-                daysReserved[i] = 4;
-            else
-                daysReserved[i] = 1;
-        } 
-    }
-
-    // Need to do something about overlaps. Might need a method to check other reservations and make sure the dates are always reserved or reuse addReservation.
-    public void cancelReservation() {
-	    int i;
-        int cID = reservation.getCheckInDate()-1;
-        int cOD = reservation.getCheckOutDate()-1;
-
-        for (i = cID; i < cOD; i++)
-                daysReserved[i] = 0;
-    }
 
     /*
      * if lets say check in and out is in the same day make a condition that it would equal to 1
