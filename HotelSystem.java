29e7ddf5274aa46fd125f7d2ab6264ac2d7556b2
DouiJ <141159120+DouiJ@users.noreply.github.com>
2024-06-21 02:10:07 +0800

    Update HotelSystem.java

--- HotelSystem.java
+++ HotelSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class HotelSystem {
 
@@ -33,52 +34,194 @@ class HotelSystem {
         return true;
     }
 
-    public boolean demolishHotel(String hotelName) {
-        // Valid inputs assumed
+    public void demolishHotel() {
+        Scanner scanner = new Scanner(System.in);
+        String hotelName, confirm;
+
+        System.out.println("Enter hotel name: ");
+        hotelName = scanner.nextLine();
+
+
         for (Hotel hotel : hotelList) {
             if (hotel.getHotelName().equals(hotelName)) {
-                hotelList.remove(hotel);
-                System.out.println("Hotel with name '" + hotelName + "' has been demolished!");
-                return true;
+                do {
+                    System.out.println("\n\nAre you sure you want to continue with this modification? (Y/N) ");
+                    confirm = scanner.nextLine();
+                } while (!confirm.equalsIgnoreCase("Y") && !confirm.equalsIgnoreCase("N"));
+
+                if (confirm.equalsIgnoreCase("Y")) {
+                    hotelList.remove(hotel);
+                    System.out.println("\nHotel with name '" + hotelName + "' has been demolished!\n");
+                }
             }
         }
-        System.out.println("Hotel with name '" + hotelName + "' has not been demolished. Please input a proper name.");
-        return false;
+        System.out.println("\nHotel with name '" + hotelName + "' has not been demolished.\n");
     }
 
-    // ASSUME INPUTS ARE VALID FOR STRING NAMES IN DRIVER/MAIN
-    public boolean changeHotelName(String currentName, String newName) {  // The user is asked in main what hotel to change already print the list there
-        for (Hotel hotel : hotelList) {
-            if (hotel.getHotelName().equals(newName)) {
+    public void changeHotelName(Hotel selectedHotel) {  // The user is asked in main what hotel to change already print the list there
+        Scanner scanner = new Scanner(System.in);
+        String newName;
+
+        do {
+            System.out.println("Enter new hotel name: ");
+            newName = scanner.nextLine();
+
+            if (selectedHotel.getHotelName().equals(newName)) // If equal to previous name
                 System.out.println("Hotel with name '" + newName + "' already exists!");
-                return false;
-            }
-        }
+            else
+                for (Hotel h : hotelList)                      // Compares to other hotels
+                {
+                    if (selectedHotel.getHotelName().equals(newName)) {
+                        System.out.println("Hotel with name '" + newName + "' already exists!");
+                        newName = null;
+                    }
+                }
+        } while (newName == null);
 
-        for (Hotel hotel : hotelList) {         // Iterate through the list to get to the index of the current hotel we want to change
-            if (hotel.getHotelName().equals(currentName)) {
-                hotel.setHotelName(newName);
-                System.out.println("Hotel with name '" + currentName + "' has been changed to '" + newName + "'!");
-                return true;
-            }
-        }
 
-        System.out.println("Hotel with name '" + currentName + "' has not been found!");
-        return false;
+        selectedHotel.setHotelName(newName);
+        System.out.println("\nHotel name has been changed to '" + newName + "'!\n");
     }
 
-    public void viewHotel(String hotelName) {
+    public Hotel selectHotel() {
+        Scanner scanner = new Scanner(System.in);
+        int choice, i;
+        Hotel selectedHotel = null;
 
+        // List of hotels
+        do {
+            System.out.println("\n Choose a hotel: ");
+            for (i = 0; i < hotelList.size(); i++)
+                System.out.println("[" + (i + 1) + "] " + hotelList.get(i).getHotelName());
+
+            System.out.println("Choose a number from the List: ");
+            choice = scanner.nextInt();
+
+            if (choice >= 1 && choice <= hotelList.size())
+                selectedHotel = hotelList.get(choice - 1);
+            else
+                System.out.println("Invalid choice!"); // selectedHotel stays null
+
+        } while (selectedHotel == null);
+
+        return selectedHotel;
     }
 
-    public void manageHotel(String hotelName) {
+    public void viewHotel() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        Hotel selectedHotel = selectHotel();
 
+        do {
+            System.out.println("""
+                    ---HOTEL VIEWER---
+
+                    [1] High-Level Information
+                    [2] Low-Level Information
+                    [0] Exit to menu
+                    """);
+
+            System.out.println("Choose a number: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    // HIGH-LEVEL
+                    System.out.println("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+                    System.out.println("\n!!!!HIGH-LEVEL INFORMATION!!!!\n");
+                    System.out.println("Name of the Hotel: " + selectedHotel.getHotelName());
+                    System.out.println("\nTotal No. of Rooms for hotel" + selectedHotel.getHotelName() + " is " + selectedHotel.getNumberOfRooms());
+                    System.out.println("\nTotal Estimated Revenue for the Month: " + selectedHotel.calculateEstimatedEarnings());
+                    System.out.println("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+                    break;
+                case 2:
+                    // LOW-LEVEL
+                /*
+                    I. Total number of available and booked rooms for a selected date (might need to make a function -- input a given start and end day to check and return if available)
+                    II. Info about selected 'room', room's name, price, and availability (print days)
+                    III. Info about selected reservation, guest info, room info, check in and out dates, total price of booking, and breakdown
+                 */
+                    System.out.println("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+                    System.out.println("\n####LOW-LEVEL INFORMATION####\n");
+
+
+                    break;
+                case 0:
+                    // EXIT
+                    System.out.println("\nExiting Hotel Viewer...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        } while (choice != 0);
     }
 
     /*
-    alot of parameters here checkin... stuff
+        Pretty much done with this one
      */
+    public void manageHotel() {
+        Scanner scanner = new Scanner(System.in);
+        int choice, numberOfRooms;
+        boolean result;
+        Hotel selectedHotel = selectHotel();
+
+        do {
+            System.out.println("\nCurrent Hotel: " + selectedHotel.getHotelName() + "\n");
+            System.out.println("""
+                    ---HOTEL MANAGER---
+
+                    [1] Change Name of Hotel
+                    [2] Add Room
+                    [3] Update Room's Base Price
+                    [4] Remove Reservation
+                    [5] Demolish Hotel
+                    [0] Exit to menu
+                    """);
+
+            System.out.println("Choose a number: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1: // Change name of hotel
+                    changeHotelName(selectedHotel);
+                    break;
+
+                case 2: // Add Room
+                    do {
+                        System.out.println("\nHow many Rooms do you want to add: ");
+                        numberOfRooms = scanner.nextInt();
+                        result = selectedHotel.addRoom(numberOfRooms);
+                    } while (!result);
+                    break;
+
+                case 3: // Remove room
+                    selectedHotel.removeRoom();
+                    break;
+
+                case 4: // Update base price of room (check conditions)
+                    selectedHotel.updateRoomPrice();
+                    break;
+
+                case 5: // Remove reservation
+                    selectedHotel.cancelReservation();
+                    break;
+
+                case 6: // Demolish hotel
+                    demolishHotel();
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        } while (choice != 0);
+    }
+
     public void simulateBooking(Hotel hotel) {
+
+        // Should be able to select a hotel and speciffy check in and check out dates
+        // Mechanism to select room (manual from list)
+        // Once a reservation is made, status of room should be updated and reservation details should be stored and viewable in viewhotel
 
     }
 
