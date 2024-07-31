684579b6571e5d998eb498d7d81700d9fd07538d
trem4ngo <tremaynedelacruz14@gmail.com>
2024-07-31 14:25:47 +0800

    Add files via upload

--- src/HRS_Driver.java
+++ src/HRS_Driver.java
@@ -1,5 +1,14 @@
 import controller.MainController;
 
+/**
+ * @author CARIAGA, Josh Enrico P., DELA CRUZ, Tremayne Hope O.
+ * Section: S18
+ * Last Modified: 30/07/2024
+ * Acknowledgements:
+ * Oracle. (n.d.). Package javax.swing. Oracle Java SE Documentation. Retrieved July 14, 2024, from https://docs.oracle.com/javase/7/docs/api/javax/swing/package-summary.html
+ * Oracle. (n.d.). Package java.awt. Oracle Java SE Documentation. Retrieved July 14, 2024, from https://docs.oracle.com/javase/7/docs/api/java/awt/package-summary.html#package_description
+ * Javatpoint. (n.d.). Java Layout Manager. Retrieved July 14, 2024, from https://www.javatpoint.com/java-layout-manager
+ */
 public class HRS_Driver {
         public static void main(String[] args) {
             MainController mainController = new MainController();
