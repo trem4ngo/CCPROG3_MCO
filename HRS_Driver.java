134a85f889edce2a261815b1964526893268cf94
trem4ngo <tremaynedelacruz14@gmail.com>
2024-07-31 12:40:28 +0800

    Delete HRS_Driver.java

--- HRS_Driver.java
+++
@@ -1,7 +0,0 @@
-public class HRS_Driver {
-    public static void main(String[] args){
-        HRS_GUI gui = new HRS_GUI();
-        HotelSystem hotelSystem = new HotelSystem();
-        HRS_Controller controller = new HRS_Controller(gui,hotelSystem);
-    }
-}
